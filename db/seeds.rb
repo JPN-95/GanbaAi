dfb827efb8256a72acb9899a5f18ce55e853d82f
james newsom <jamespnews95@gmail.com>
2026-01-27 21:18:54 +0900

    seed initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "creating db:)"
+
+User.new(id:1, email: "patnew1@gmail.co", password:"bigsecret", username:"lol")
+Test.new(id:1, title: "grammar", user_id:1)
+Test.new(id:2, title: "vocab", user_id:1)
+Question.new(id:1, question: "", generated_answers:[], user_answer:"", test_id:1)
