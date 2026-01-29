7fdf1fae3a4d041483377ad0d36b6da2575e9290
Katie Wood <kwood6319@hotmail.co.uk>
2026-01-29 21:53:38 +0900

    Added method for checking if test is complete

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :user
   has_many :questions
+  validates :title, presence: true
+  validates :category, presence: true, inclusion: { in: ["Vocabulary", "Grammar", "Reading", "Kanji"]}
+
+  def complete?
+    !questions.where(user_answer: "").exists?
+  end
 end
