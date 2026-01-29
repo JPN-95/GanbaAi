92bbe9ada5a9cb5c7d38bf674e860d07b869364d
Renato Shimabukuro <renato.shimabukuro.2@gmail.com>
2026-01-29 21:15:54 +0900

    working in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])]
+    # @user = User.find(current_user)
   end
   def dashboard
+    @user_id = current_user.id
+    @test = Test.find(params[:user_id])
+  end
+
+  def progress
+    @test = Test.find(params[:user_id])
 
   end
+
 
   def set_time_zone
     Time.zone = current_user.time_zone
+  end
+
+  private
+  def params_test
+    require(:user).permit(:user_id)
   end
 end
