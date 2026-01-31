f9cbb9309bdb604126c6bc163b348115cd257a17
Troy Zangara <troyzangara@gmail.com>
2026-01-31 13:42:49 +0900

    Limit set_test callback to edit and update actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_test
+  before_action :set_test, only: [:edit, :update]
   before_action :set_question, only: [:edit, :update]
 
 def edit
