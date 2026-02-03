d7cce39e1d191138bf65084544d0d8eacdb7f12b
james newsom <jamespnews95@gmail.com>
2026-02-03 21:39:37 +0900

    tools

--- app/tools/jlpt_question_tool.rb
+++ app/tools/jlpt_question_tool.rb
@@ -6,9 +6,13 @@ class JlptQuestionTool < RubyLLM::Tool
   param :category, desc: "either vocabulary, grammar, kanji, reading"
 
   def execute(level:, category:)
-    url = "https://japanesetest4you.com/jlpt-n#{level}-#{category}-exercise-4,"
+    url = "https://japanesetest4you.com/japanese-language-proficiency-test-jlpt-n#{level}-#{category}-exercise-4/"
+    puts url
+    response = URI.parse(url).read({
+                                          'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36', 'Accept-Language' => 'en'
+                                        })
 
-    doc = Nokogiri::HTML(URI.parse(url).read({'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36', 'Accept-Language' => 'en'}))
+    doc = Nokogiri::HTML(response)
     doc.search("div.entry.clearfix")
 
   rescue => e
