f86fa9507ba4e41f03838c8d321005fe23b6a460
Troy Zangara <troyzangara@gmail.com>
2026-02-07 14:30:44 +0900

    Add foreign keys for active storage attachments

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_05_125742) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "users"
 end
